e53ab33efe642bd07e4bf39f97f14a55f8847235
Matan <matanhakim@gmail.com>
2022-12-02 14:25:07 +0200

    Day 01 part 2

--- day01.R
+++ day01.R
@@ -1,6 +1,7 @@
 calories <- readLines("inputs/day01.txt")
 calories <- as.numeric(calories)
 
+# Part one
 max_cal <- 0
 cal_count <- 0
 for (i in calories){
@@ -13,3 +14,17 @@ for (i in calories){
     cal_count <- cal_count + i
   }
 }
+
+# Part two
+library(tidyverse)
+input <- tibble(calories) %>% 
+  mutate(elf = cumsum(is.na(calories)))
+
+output <- input %>% 
+  drop_na(calories) %>% 
+  group_by(elf) %>% 
+  summarise(calories = sum(calories)) %>% 
+  arrange(desc(calories)) %>% 
+  slice_head(n = 3) %>% 
+  pull() %>% 
+  sum()
